d2d15855c968992571fa9823537b912db379520b
sohxxny <sohxxny@gmail.com>
2025-02-27 00:42:28 +0900

    feat: 인증 여부에 따른 헤더 버튼 표시 (#10)

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <header className="fixed h-28 w-full">Header</header>
-  )
-}
+  const [isLogin] = useState(true);
 
-export default Header
+  return (
+    <header className="fixed h-28 w-full">
+      {isLogin ? (
+        <Link to="/signin">로그인</Link>
+      ) : (
+        <>
+          <Link to="/mypage">마이페이지</Link>
+          <button>로그아웃</button>
+        </>
+      )}
+    </header>
+  );
+};
+
+export default Header;
