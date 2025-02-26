17d0df56658b1d6e3b421245b39667733f3f269c
woohyuckk <woohyuckk@gmail.com>
2025-02-26 19:34:17 +0900

    feat:#1 Router 추가

---
+++ src/pages/Mypage.jsx
@@ -0,0 +1,8 @@
+
+const Mypage = () => {
+  return (
+    <div>Mypage</div>
+  )
+}
+
+export default Mypage
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import Layout from "../components/layout/Layout";
+import Detail from "../pages/Detail";
+import Mypage from "../pages/Mypage";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route element={<Layout/>}>
+        <Route path="/" element={<Home />}/>
+        <Route path="/detail" element={<Detail/>}/>
+        <Route path="/mypage" element={<Mypage />}/>
+        <Route path="/signin" element={<SignIn />}/>
+        <Route path="/signup" element={<SignUp />}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
