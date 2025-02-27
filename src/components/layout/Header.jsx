078ce635092a8cececdde5417062e6bea22f55fc
sohxxny <sohxxny@gmail.com>
2025-02-27 21:24:30 +0900

    refactor: 헤더에 authStore 호출 (#22)

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import useAuthStore from '../../stores/useAuthstore';
 
 const Header = () => {
-  const [isLogin] = useState(true);
+  const { isLogin } = useAuthStore();
 
   return (
     <header className="fixed h-28 w-full">
-      {!isLogin ? (
-        <Link to="/signin">로그인</Link>
-      ) : (
-        <>
-          <Link to="/mypage">마이페이지</Link>
-          <button>로그아웃</button>
-        </>
-      )}
+      <nav className="flex px-15 h-full items-center">
+        <h1 className="flex-grow">
+          <Link to="/">로고</Link>
+        </h1>
+        {isLogin ? (
+          <div className="flex gap-5">
+            <Link to="/mypage">마이페이지</Link>
+            <button>로그아웃</button>
+          </div>
+        ) : (
+          <Link to="/signin">로그인</Link>
+        )}
+      </nav>
     </header>
   );
 };
