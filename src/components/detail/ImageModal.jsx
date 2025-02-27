4ac774d7ab65b40a9cc0a158bc7991ee00fd4092
woohyuckk <woohyuckk@gmail.com>
2025-02-27 15:15:42 +0900

    design: 모달창 이미지 크기 조정 (#4)

--- src/components/detail/ImageModal.jsx
+++ src/components/detail/ImageModal.jsx
@@ -1,17 +1,17 @@
 const ImageModal = ({ handleModalClose: closeModal, modalImage }) => {
   return (
     <div
-      className="fixed inset-0 bg-grey bg-opacity-20 flex items-center justify-center z-50 backdrop-blur-md"
+      className="fixed inset-0 bg-amber-100 bg-amber-100/20 flex items-center justify-center z-50 backdrop-blur-md"
       onClick={closeModal}
     >
-      <div className="relative bg-white p-4 rounded-lg shadow-lg min-w-1/2 min-h-1/2">
+      <div className="relative bg-white p-4 rounded-lg shadow-lg w-1/3 ">
         <button
           className="absolute top-2 right-2 bg-red-500 text-white p-2  w-8 h-8 rounded-full flex items-center justify-center  hover:bg-red-700"
           onClick={closeModal}
         >
           ✖
         </button>
-        <img src={modalImage} alt="modal" className="w-full h-auto rounded-lg" />
+        <img src={modalImage} alt="modal" className="w-full h-full rounded-lg" />
       </div>
     </div>
   );
--- src/components/detail/PlaceSection.jsx
+++ src/components/detail/PlaceSection.jsx
@@ -11,7 +11,7 @@ const PlaceSection = ({ handleModalOpen: openModal, placeInfo }) => {
   const [open, close, parking, isAccompanied, exption] = description;
 
   return (
-    <div className="w-full md:w-2/3 bg-white rounded-xl shadow-lg p-6 border border-gray-200">
+    <div className="w-full md:w-2/3 h-auto felx flex-col bg-white rounded-xl shadow-lg p-6 border border-gray-200">
       <h1 className="text-2xl font-bold text-gray-800">{placeInfo.title}</h1>
 
       {/* 카테고리 */}
@@ -25,11 +25,11 @@ const PlaceSection = ({ handleModalOpen: openModal, placeInfo }) => {
       </div>
 
       {/* 대표 이미지 */}
-      <div className="mt-4 flex">
+      <div className="mt-4 flex justify-center">
         <img
           src="/public/default-image.png"
           alt="default-image"
-          className="w-full md:w-1/2 mx-auto aspect-square object-contain rounded-lg shadow-md hover:scale-105 transition-all duration-300 cursor-pointer"
+          className="w-full md:w-1/2  object-cover rounded-lg shadow-md hover:scale-105 transition-all duration-300 cursor-pointer"
           onClick={() => openModal('/public/default-image.png')}
         />
       </div>
@@ -41,18 +41,19 @@ const PlaceSection = ({ handleModalOpen: openModal, placeInfo }) => {
           <img
             src="/public/default-image.png"
             alt="default-image"
-            className="w-full h-full object-cover rounded-lg shadow-md hover:scale-110 transition-all duration-300 cursor-pointer"
+            className="w-full h-full object-cover rounded-lg shadow-md  cursor-pointer"
           />
         </div>
 
         {/* 정보 텍스트 */}
-        <div className="flex flex-col gap-2 text-gray-700">
+        <div className="w-full md:w-1/2 flex flex-col gap-2 text-gray-700">
           <p>⏰ {open}</p>
           <p>🚪 {close}</p>
           <p>📍 {placeInfo.address}</p>
           <p>📞 전화번호:{placeInfo.tel || '❌'}</p>
           <p>💡 {isAccompanied}</p>
           <p>⚠️ {exption}</p>
+          <p>🚗 {parking}</p>
         </div>
       </div>
     </div>
