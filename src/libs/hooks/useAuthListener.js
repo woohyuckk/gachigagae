e1c43809059c329f2943f5220cd58259095ace9e
ye21iin <kathykr120@gmail.com>
2025-02-27 20:11:41 +0900

    feat: extraUserInfo fetching logic in progress(inside useEffect)

--- src/libs/hooks/useAuthListener.js
+++ src/libs/hooks/useAuthListener.js
@@ -10,11 +10,39 @@ const useAuthListener = () => {
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_, session) => {
-      console.log(session);
       const userInfo = {
         id: session?.user.id,
-        email: session?.user.user_metadata.email,
+        email: session?.user.email,
       };
+
+      //  public.users 추가 정보 가져오기 (nickname, profile_img_url) ---> tanstack 수정 필요
+      async function getExtraUserInfo(userId) {
+        try {
+          const { data, error } = await supabase
+            .from('users')
+            .select('nickname, profile_img_url')
+            .eq('id', userId);
+
+          if (error) {
+            throw new Error(error);
+          }
+
+          return {
+            nickname: data.nickname || '',
+            profile_img_url: data.profile_img_url || '',
+          };
+        } catch (error) {
+          console.error('Error fetching user info from public.users:', error.message);
+          return { nickname: '', profile_img_url: '' };
+        }
+      }
+
+      if (session?.user.id) {
+        const { nickname, profile_img_url } = getExtraUserInfo(session.user.id);
+        userInfo.nickname = nickname;
+        userInfo.profile_img_url = profile_img_url;
+      }
+
       setUserInfo(userInfo);
       return () => {
         subscription.unsubscribe(); // Cleanup when unmounting
