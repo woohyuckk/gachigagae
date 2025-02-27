0a748775ca338f07f32c10755e8ccfac2df1791f
sohxxny <sohxxny@gmail.com>
2025-02-28 01:33:26 +0900

    feat: 로그아웃 버튼에 함수 추가 (#22)

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,6 +3,7 @@ import useAuthStore from '../../stores/useAuthstore';
 
 const Header = () => {
   const { isLogin } = useAuthStore();
+  const logout = useAuthStore((state) => state.logout);
 
   return (
     <header className="fixed h-28 w-full">
@@ -13,7 +14,9 @@ const Header = () => {
         {isLogin ? (
           <div className="flex gap-5">
             <Link to="/mypage">마이페이지</Link>
-            <button>로그아웃</button>
+            <button onClick={logout} className="cursor-pointer">
+              로그아웃
+            </button>
           </div>
         ) : (
           <Link to="/signin">로그인</Link>
