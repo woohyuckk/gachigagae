162b250a11230c33220f9c3acd964bd4fd7b2b4a
K-jisu <wltn7star@naver.com>
2025-03-01 00:11:53 +0900

    ✨feat : react-intersection-observer 기능 추가

--- src/components/home/HomeCardContainer.jsx
+++ src/components/home/HomeCardContainer.jsx
@@ -4,10 +4,23 @@ import SideBar from './SideBar';
 import HOME_CONSTANT from '../../constants/homeConstant';
 import { useNavigate } from 'react-router-dom';
 import homeUtils from '../../libs/utils/homeUtils';
+import { useInView } from 'react-intersection-observer';
+import useInfinitePlaces from '../../libs/hooks/useInfinitePlaces';
 
 const HomeCardContainer = ({ getPlaces }) => {
   const [places, setPlaces] = useState(getPlaces);
   const navigate = useNavigate();
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isFetching } = useInfinitePlaces();
+
+  const [ref, inView] = useInView({
+    threshold: 0.3,
+    onChange: (inView) => {
+      if (!inView || !hasNextPage || !isFetchingNextPage) return;
+      fetchNextPage();
+    },
+  });
+  console.log(data);
 
   // 카테고리 정렬
   const handleCategory = (e) => {
@@ -23,6 +36,8 @@ const HomeCardContainer = ({ getPlaces }) => {
       homeUtils.scrollToTop();
     }
   };
+
+  if(isFetching) return(<div>Fetching...</div>)
 
   return (
     <div className="lg:w-full lg:max-w-3xl m-auto flex flex-wrap gap-7 justify-evenly p-4 sm:w-1/2 md:gap-20">
@@ -41,7 +56,17 @@ const HomeCardContainer = ({ getPlaces }) => {
                 : 'lg:w-1/3 sm:w-full w-3/4 h-80 bg-white shadow-lg rounded-2xl overflow-hidden p-4 relative flex-col lg:top-16  '
             }
           >
-            <HomeCard place={place} onClick={handleCategory} />
+            {idx % 7 === 0 && idx !== 0 ? (
+              <HomeCard
+                place={place}
+                onClick={handleCategory}
+                ref={ref}
+                inView={inView}
+                text="7번째"
+              />
+            ) : (
+              <HomeCard place={place} onClick={handleCategory} />
+            )}
           </article>
         );
       })}
