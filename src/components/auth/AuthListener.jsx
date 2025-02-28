b749079b19aeaad883836827bf75c8c9e19251bd
ye21iin <kathykr120@gmail.com>
2025-02-28 19:24:31 +0900

    fix: 전역상태 예외처리 로직 추가 #3

--- src/components/auth/AuthListener.jsx
+++ src/components/auth/AuthListener.jsx
@@ -5,29 +5,34 @@ import useGetUserInfo from '../../libs/hooks/useGetUserInfo';
 
 const AuthListener = () => {
   const setUserInfo = useAuthStore((state) => state.setUserInfo);
-  const { data: userData, isPending } = useGetUserInfo();
+  const logout = useAuthStore((state) => state.logout);
+  const { data: userData, isPending, Error } = useGetUserInfo();
 
   useEffect(() => {
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange(async (_, session) => {
-      const userInfo = {
-        id: session.user.id,
-        email: session.user.email,
-        nickname: userData?.nickname || '',
-        profile_img_url: userData?.profile_img_url || '',
-      };
+      if (session) {
+        const userInfo = {
+          id: session.user.id,
+          email: session.user.email,
+          nickname: userData?.nickname || '',
+          profile_img_url: userData?.profile_img_url || '',
+        };
 
-      setUserInfo(userInfo);
+        setUserInfo(userInfo);
+      } else {
+        logout();
+      }
     });
-
     // cleanUp
     return () => {
       subscription.unsubscribe();
     };
   }, [userData]); // userData 변경 시 리렌더링
 
-  if (isPending) return <>기다려.</>;
+  if (isPending) return <>pending...</>;
+  if (Error) return <>error...</>;
 
   return <></>;
 };
