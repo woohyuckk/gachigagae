127d7e645d38f79ceb22480b6d872625c94f6930
woohyuckk <woohyuckk@gmail.com>
2025-03-01 20:09:10 +0900

    refact: image modal 창 크기 조절 (#4)

--- src/components/detail/ImageModal.jsx
+++ src/components/detail/ImageModal.jsx
@@ -1,10 +1,10 @@
 const ImageModal = ({ handleModalClose: closeModal, modalImage }) => {
   return (
     <div
-      className="fixed inset-0 bg-amber-100/20 flex items-center justify-center z-50 backdrop-blur-md"
+      className="fixed  inset-0 bg-amber-100/20 flex items-center justify-center z-50 backdrop-blur-md"
       onClick={closeModal}
     >
-      <div className="relative bg-white p-4 rounded-lg shadow-lg w-1/3 ">
+      <div className="relative  bg-white p-4 rounded-lg shadow-lg w-[50vw] ">
         <button
           className="absolute top-2 right-2 bg-red-500 text-white p-2  w-8 h-8 rounded-full flex items-center justify-center  hover:bg-red-700"
           onClick={closeModal}
