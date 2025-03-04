430a74833a4860321fca5400176934eb48f6b0aa
woohyuckk <woohyuckk@gmail.com>
2025-03-04 12:29:01 +0900

    chore: prettier 미적용 컴포넌트 적용 (#4)

--- src/components/detail/ImageModal.jsx
+++ src/components/detail/ImageModal.jsx
@@ -11,7 +11,11 @@ const ImageModal = ({ handleModalClose: closeModal, modalImage }) => {
         >
           ✖
         </button>
-        <img src={modalImage} alt="modal" className="w-full  max-h-[600px] md:w-full  object-cover rounded-lg" />
+        <img
+          src={modalImage}
+          alt="modal"
+          className="w-full  max-h-[600px] md:w-full  object-cover rounded-lg"
+        />
       </div>
     </div>
   );
