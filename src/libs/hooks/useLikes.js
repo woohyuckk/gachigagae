f4ee5352be5e5bd95e78a8ef5a059e4c6ecb3776
sohxxny <sohxxny@gmail.com>
2025-03-04 11:49:23 +0900

    fix: optimistic update 적용되지 않는 버그 수정 (#44)

--- src/libs/hooks/useLikes.js
+++ src/libs/hooks/useLikes.js
@@ -21,42 +21,41 @@ export const useGetLikes = (userId) => {
  * * 좋아요를 추가하거나 삭제하는 커스텀 훅
  * @param {boolean} isLiked - 현재 장소의 좋아요 여부
  * @param {string} userId - 현재 로그인한 유저의 uuid
+ * @param {string} selectedCategory - 현재 카테고리 (카테고리 관련 없을 시 null 전달)
  * @return {Function} return.toggleLike - 좋아요 토글 mutation 함수
  */
 export const useToggleLikes = (isLiked, userId, selectedCategory) => {
   const queryClient = useQueryClient();
   const queryKey = ['infinitePlaces', userId, selectedCategory];
 
-  // * 옵티미스틱 업데이트를 위한 onMutate 함수
-  const handleMutate = async ({ placeId }) => {
-    // 진행중인 쿼리 취소 및 현재 상태 저장
-    await queryClient.cancelQueries({ queryKey });
-    const previousData = queryClient.getQueryData(queryKey).pages;
-    // 클라이언트(캐시) 상태 업데이트
-    queryClient.setQueryData(queryKey, (oldData) => {
-      return {
-        ...oldData,
-        pages: oldData.pages?.map((pages) => {
-          return pages.map((place) => {
-            if (place.id === placeId) {
-              return {
-                ...place,
-                isLiked: !place.isLiked,
-              };
-            } else {
-              return place;
-            }
-          });
-        }),
-      };
-    });
-    return { previousData };
-  };
-
   // mutation 정의
   const toggleLikeMutation = useMutation({
     mutationFn: isLiked ? deleteLikes : addLikes,
-    onMutate: handleMutate,
+    // * 옵티미스틱 업데이트를 위한 onMutate 함수
+    onMutate: async ({ placeId }) => {
+      // 진행중인 쿼리 취소 및 현재 상태 저장
+      await queryClient.cancelQueries({ queryKey });
+      const previousData = queryClient.getQueryData(queryKey);
+      // 클라이언트(캐시) 상태 업데이트
+      queryClient.setQueryData(queryKey, (oldData) => {
+        return {
+          ...oldData,
+          pages: oldData.pages?.map((pages) => {
+            return pages.map((place) => {
+              if (place.id === placeId) {
+                return {
+                  ...place,
+                  is_liked: !isLiked,
+                };
+              } else {
+                return place;
+              }
+            });
+          }),
+        };
+      });
+      return { previousData };
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['infinitePlaces', userId] });
     },
